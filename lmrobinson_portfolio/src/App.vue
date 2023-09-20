54c47e72237a688ac4df459aa19a069d92c5b734
Lachlan Robinson <lachy4242@hotmail.com>
2023-09-20 18:49:40 +1000

    First commit

---
+++ lmrobinson_portfolio/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="grid-container">
+        <div id="header">
+            <MainHeader />
+        </div>
+        <div id="contactbar">  
+            <ContactLinks />
+        </div>
+        <div id="navbar">  
+            <NavBar/>
+        </div>
+        <div id="content">
+            <HomeContent/>
+        </div>
+    </div>
+    
+    
+</template>
+
+<script>
+    import MainHeader from './components/MainHeader.vue'
+    import ContactLinks from './components/ContactLinks.vue'
+    import NavBar from './components/NavBar.vue'
+    import HomeContent from './components/HomeContent.vue'
+
+    export default {
+        name: 'App',
+        components: {MainHeader,ContactLinks,NavBar,HomeContent},
+    }
+</script>
+
+<style>
+    body {
+        margin:0;
+        height:100vh;
+    }
+
+    .grid-container {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        grid-template-columns: 10vw 40vw 45vw 5vw;
+        grid-template-rows: auto auto auto;
+    }
+
+    #header {
+        grid-column-start: 1;
+        grid-column-end: 5;
+        grid-row-start:1;
+        grid-row-end:1;
+    }
+
+    #contactbar {
+        grid-column-start: 4;
+        grid-column-end: 5;
+        grid-row-start: 2;
+        grid-row-end: 3;
+        height: 100%;
+    }
+
+    #navbar {
+        grid-column-start: 1;
+        grid-column-end: 2;
+        grid-row-start: 2;
+        grid-row-end: 2;
+    }
+
+    #content {
+        grid-column-start: 2;
+        grid-column-end: 4;
+        grid-row-start: 2;
+        grid-row-end: 2;
+    }
+</style>
---
+++ lmrobinson_portfolio/src/components/ContactLinks.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <a href="https://www.facebook.com/Lachlan.Robinson/" target="_blank">
+            <img src="../assets/fb_plain.png">
+        </a>
+        <a href="https://www.instagram.com/lachlanrobinson_/" target="_blank">
+            <img src="../assets/insta_plain.png">
+        </a>
+        <a href="https://github.com/KL1X-AU" target="_blank">
+            <img src="../assets/github_plain.png">
+        </a>
+    </div>
+    
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    div {
+        padding: 1vw;
+        background-color: #313131;
+        height: 100%;
+    }
+    a {
+        display: block;
+        height: 4vw;
+        width: 3vw;
+        position:relative;
+    }
+
+    img {
+        width: 3vw;
+        margin: auto;
+        top:0;
+        bottom:0;
+        left:0;
+        right:0;
+        margin: auto;
+        position: absolute;
+        transition: 0.2s-;
+    }
+    img:hover {
+        width: 3.5vw;
+        transition:0.2s;
+    }
+
+</style>
